9bc656419707681073bddc79c8e6eac40a77b7e3
Gennadiy Pokidov <faqvsem2@yandex.ru>
2018-10-06 01:04:07 +0700

    add first request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'stats';
+export class AppComponent implements OnInit {
+
+  returnData: any = [];
+  leagueNames: Array<string> = [];
+  leagueName: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://api.opendota.com/api/leagues?api_key=5314a5f5-e926-4eae-8fe4-fb1f9cbbd20c').subscribe((data) => {
+      this.returnData = data;
+      this.addLeagueNames(this.returnData);
+    });
+    const aa: any = this.http.get('https://api.opendota.com/api/scenarios/itemTimings?api_key=5314a5f5-e926-4eae-8fe4-fb1f9cbbd20c');
+
+    forkJoin([aa]).subscribe(results => {
+      console.log(results[0]);
+    });
+  }
+
+  addLeagueNames(data: any) {
+    for (let i = 0; i < data.length; i++) {
+      this.leagueNames.push(data[i]);
+    }
+  }
+
+  // getData(): any {
+  //   this.http.get('https://api.opendota.com/api/leagues?api_key=5314a5f5-e926-4eae-8fe4-fb1f9cbbd20c').subscribe((data) => {
+  //     return data;
+  //   });
+  // }
+
 }
